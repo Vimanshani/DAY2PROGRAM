e5c46f6993b527354039003443e9805937af2be5
Vimanshani <pabodhavimashani@gmail.com>
2023-05-27 09:49:25 +0530

    1

---
+++ firstprogramday2.js
@@ -0,0 +1,4 @@
+let one= 10;
+let two= 8;
+console.log(one % two);
+console.log(one ** two);
---
+++ secondprogramday2.js
@@ -0,0 +1,4 @@
+console.log("Hello," + "World!");
+console.log((-1)*(-10)*(-20));
+console.log(2*(4+5*(6+3)));
+console.log(36/3+4*3);
